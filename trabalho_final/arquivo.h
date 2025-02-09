6ae79c072a2a5f3babe544e6724f5f942f85fe75
rodrigolyrio <rodrigolyrio2009@gmail.com>
2025-02-09 12:29:29 -0300

    Update arquivo.h

--- trabalho_final/arquivo.h
+++ trabalho_final/arquivo.h
@@ -3,8 +3,10 @@
 
 #include "bd_paciente.h"
 
+// Função para ler os registros de pacientes de um arquivo CSV, inserindo-os logo depois na estrutura de dados do sistema.
 void carregarDadosPacientes(BDPaciente *bd, const char *caminhoArquivo);
+
+// Função responsável por salvar os registros de pacientes da estrutura de dados em um arquivo CSV, sobrescrevendo o conteúdo anterior em seguida.
 void salvarDadosPacientes(BDPaciente *bd, const char *caminhoArquivo);
 
 #endif
-// ja modificado (nao precisa)

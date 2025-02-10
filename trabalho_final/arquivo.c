2b37d6a7910ff0d6955ae55ea8439c4b49815336
rodrigolyrio <rodrigolyrio2009@gmail.com>
2025-02-09 22:37:24 -0300

    Update arquivo.c

--- trabalho_final/arquivo.c
+++ trabalho_final/arquivo.c
@@ -13,50 +13,52 @@ void carregarDadosPacientes(BDPaciente *bd, const char *caminhoArquivo) {
     while (fgets(linha, MAX_LINHA, arquivo)) {
         if (strlen(linha) < 5)
             continue; 
-        char *campo = strtok(linha, ",");
-        if (!campo)
+        // Leitura do campo id (primeiro campo)
+        char *campo = strtok(linha, ","); // Separa os elementos com base na 'divisoria' virgula
+        if (!campo) // Se não conseguir extrair o campo, pula a linha
             continue;
-        int id = atoi(campo);
+        int id = atoi(campo); // Transforma a string em inteiro, armazenando-o em id
         
-        campo = strtok(NULL, ",");
+        campo = strtok(NULL, ","); // Lendo o segundo campo (cpf)
         if (!campo)
             continue;
         char cpf[MAX_STR];
-        strcpy(cpf, campo);
+        strcpy(cpf, campo); // Copia o conteudo do campo para a variavel cpf
         
         campo = strtok(NULL, ",");
         if (!campo)
             continue;
-        char nomePaciente[MAX_STR];
-        strcpy(nomePaciente, campo);
+        char nomePaciente[MAX_STR]; // Cria a variavel nomePaciente para armazenar o conteudo do campo obtido no arquivo
+        strcpy(nomePaciente, campo); // Armazena o nome do paciente
         
-        campo = strtok(NULL, ",");
+        campo = strtok(NULL, ","); // Separa o campo 'idade'
         if (!campo)
             continue;
         int idade = atoi(campo);
         
-        campo = strtok(NULL, "\n");
+        campo = strtok(NULL, "\n"); // Separa o campo que contém data de cadastro
         if (!campo)
             continue;
         char dataCadastro[MAX_STR];
         strcpy(dataCadastro, campo);
         
         Paciente *novoPaciente = criarPaciente(id, cpf, nomePaciente, idade, dataCadastro);
-        inserirPaciente(bd, novoPaciente);
+        // No código acima, cada campo separado anteriormente é 'levado' para a função a fim de criar o novo nó e inserindo na lista
+        inserirPaciente(bd, novoPaciente); // O novo nó criado é inserido na lista encadeada
     }
     fclose(arquivo);
 }
 
 void salvarDadosPacientes(BDPaciente *bd, const char *caminhoArquivo) {
-    FILE *arquivo = fopen(caminhoArquivo, "w");
+    FILE *arquivo = fopen(caminhoArquivo, "w"); 
     if (arquivo == NULL)
         return;
     Paciente *atual = bd->inicio;
-    while (atual) {
-        fprintf(arquivo, "%d,%s,%s,%d,%s\n",
+    while (atual) { // Percorre toda a lista encadeada criada 
+        fprintf(arquivo, "%d,%s,%s,%d,%s\n", 
                 atual->id, atual->cpf, atual->nome, atual->idade, atual->dataCadastro);
         atual = atual->proximo;
     }
+    // Escreve todos os registros no arquivo, salvando-os e atualizando o banco de dados
     fclose(arquivo);
 }
-//ja modificado (olhar depois)
